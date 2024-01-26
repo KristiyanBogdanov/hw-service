4d89e1dbbda86130c8a62f2538302be5bcb11eb0
Кристиян Мирославов Богданов <71849513+KristiyanBogdanov@users.noreply.github.com>
2024-01-26 15:29:12 +0200

    feat: add mobile app notifications

--- hw-api/src/weather-station/weather-station-schedule.service.ts
+++ hw-api/src/weather-station/weather-station-schedule.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { WEATHER_STATION_CRON_INTERVAL_IN_MILLISECONDS, WEATHER_STATION_CRON_PATTERN } from '../shared/constants';
+import { DeviceType, Importance, NotificationType } from '../abstract-device/enum';
 import { WeatherStationRepository } from './repository';
+import { WeatherStationService } from './weather-station.service';
 
 @Injectable()
 export class WeatherStationScheduleService {
-    constructor(private readonly weatherStationRepository: WeatherStationRepository) { }
+    constructor(
+        private readonly weatherStationRepository: WeatherStationRepository,
+        private readonly weatherStationService: WeatherStationService
+    ) { }
 
     @Cron(WEATHER_STATION_CRON_PATTERN)
     async updateIsActiveStatus() {
@@ -17,6 +22,14 @@ export class WeatherStationScheduleService {
         });
         
         weatherStations.forEach(weatherStation => {
+            const report = {
+                serialNumber: weatherStation.serialNumber,
+                importance: Importance.Warning,
+                generalMessage: 'Weather station is inactive',
+                timestamp: new Date()
+            };
+
+            this.weatherStationService.sendNotificationToMobileApp(report, DeviceType.WeatherStation, NotificationType.InactiveDevice);
             console.log(`Weather station with serial number ${weatherStation.serialNumber} is inactive`);
 
             weatherStation.isActive = false;
